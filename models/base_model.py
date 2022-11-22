938fe438f92506b80e281849a5f4cea94097a488
Hardbass1991 <midailsorridente@gmail.com>
2022-11-21 16:41:46 -0800

    added feature that allows to create instance of object with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,22 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
+        """Instantiates a new model"""
+        if not kwargs or 'id' not in kwargs.keys():
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            if kwargs:
+                self.__dict__.update(kwargs)
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], 
+                    '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], 
+                    '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+            
             self.__dict__.update(kwargs)
 
     def __str__(self):
