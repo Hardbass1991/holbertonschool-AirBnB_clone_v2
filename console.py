23b3225b2c4caf4d85c9e153f2c856604a8722e7
DaisyGeraldine <chipanal@hotmail.com>
2022-11-26 17:58:45 -0500

    Fix: modify column table lastname and first name as NULL

--- console.py
+++ console.py
@@ -227,7 +227,6 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
-        print(storage.all())
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
