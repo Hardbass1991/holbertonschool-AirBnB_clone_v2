9109c66e614cbfe2264640d160adf230954600ba
DaisyGeraldine <chipanal@hotmail.com>
2022-11-26 22:00:07 -0500

    fix: modify city file, the conditional of the BaseModel class

--- models/city.py
+++ models/city.py
@@ -6,17 +6,19 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    if storage_type == "db":
+if storage_type == "db":
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
         state = relationship("State", back_populates="cities")
         places = relationship("Place", back_populates="cities",
-            cascade="all, delete")
+                              cascade="all, delete")
 
-    else:
+else:
+    class City(BaseModel):
+        """ The city class, contains state ID and name to BaseModel"""
         name = ""
         state_id = ""
