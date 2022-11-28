f1dfe63b4beb069ca0bc2c44c2a9e2e209d2d123
Hardbass1991 <midailsorridente@gmail.com>
2022-11-28 20:14:32 +0000

    added view

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """View that shows a greeting"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """View that literally displays 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text=""):
+    """View that displays 'C' followed by input string
+
+    Keyword arguments:
+    text - input text
+    """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """View that display 'Python' followed by input text"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
