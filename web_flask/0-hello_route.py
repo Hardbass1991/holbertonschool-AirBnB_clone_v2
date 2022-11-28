67ded3a6179a7599469c3e0af88aa5313619eabe
Hardbass1991 <midailsorridente@gmail.com>
2022-11-28 18:13:56 +0000

    added codumentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Module that starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+	"""View that shows a greeting"""
 	return "Hello HBNB!"
 
 if __name__ == "__main__":
