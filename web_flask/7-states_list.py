ab8b1f1b0a092405dca024204b62e347967d5c17
Hardbass1991 <midailsorridente@gmail.com>
2022-11-29 20:50:41 +0000

    added a web application that displays a view with data from storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,17 @@
+from models import storage, storage_type
+from models.state import State
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+@app.route("/states_list", strict_slashes=False)
+def number_template():
+    """View that renders a template with all State instances"""
+    states = list(storage.all(State).values())
+    storage.close()
+
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
