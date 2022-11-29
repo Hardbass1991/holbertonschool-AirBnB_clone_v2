e712f3573e67e4aff70b5ca3fa45b4196030ea06
Hardbass1991 <midailsorridente@gmail.com>
2022-11-29 04:15:13 +0000

    corrected spacing

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -56,7 +56,7 @@ def number_odd_or_even(n):
     if not n.isnumeric():
         abort(404)
     return render_template('6-number_odd_or_even.html', n=int(n))
-    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
