b1d5cdb1dd06ba5e401068b989e7cce1176c79a0
Hardbass1991 <midailsorridente@gmail.com>
2022-11-30 18:13:42 +0000

    added view that displays templates with data from storage, every state and every city in it, in alphabetical order

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that displays template with data from storage"""
+from models import storage, storage_type
+from models.city import City
+from models.state import State
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """View that renders a template with all City instances by State"""
+    d = {}
+    states = storage.all(State).values()
+    for state in states:
+        d[state.name] = {}
+        d[state.name]["id"] = state.id
+        if storage_type == "db":
+            d[state.name]["cities"] = state.cities
+        else:
+            d[state.name]["cities"] = state.cities()
+            
+    storage.close()
+
+    return render_template('8-cities_by_states.html', d=d)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
